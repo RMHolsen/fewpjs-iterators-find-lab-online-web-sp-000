72962f05e842171f7bc9a65abcd2b340f608ad86
rmholsen <jaguar.kitty@gmail.com>
2021-06-07 12:28:46 -0400

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,26 @@
+/* Why the hell is this even here 
+
 const testVar = {}
 
 function testFunc() {
   return "hi"
+}
+
+*/
+
+const superbowlWin = (stats) => {
+// function! stats here used to = the W/L stats for a team
+  let result = stats.find(chiefsWin)
+  // assign the variable result to the result 
+  return !!result ? result.year : undefined 
+  // return either the year of the result or undefined depending on whether or not they won that year
+  // IT'S CALLED A TERNARY OPERATOR
+}
+
+const chiefsWin = (game) => {
+  if (game.result === 'W') {
+    return game 
+  }
+  // if the game result was a win, return the game object
+  // also remember your damn javascript syntax please
 }
